a1bdf79fd3abcda100020c9d2ed1850c56ca8fd3
Lane Anderson <lane.anderson@moody.edu>
2017-09-27 18:30:29 -0500

    Added working game functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,89 +20,115 @@ var trivia = [{
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "How many square miles were destroyed in the Great Chicago Fire of 1871?",
+	"Answer": "4",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "2",
+		2: "3",
+		3: "4",
+		4: "5"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "How many  NFL Championships have the Chicago Bears won? ",
+	"Answer": "Nine",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "Fine",
+		2: "Seven",
+		3: "Nine",
+		4: "Eleven"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "How many Stanley Cups have the Chicago Blackhawks won?",
+	"Answer": "Five",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "None",
+		2: "One",
+		3: "Four",
+		4: "Five"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "In what year was Chicago the host of The Century of Progress Worlds' Fair?",
+	"Answer": "1933",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "1928",
+		2: "1931",
+		3: "1932",
+		4: "1933"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "Illinois leads all other US states in the amount of electricity that it generates from what type of fuel?",
+	"Answer": "Nuclear Power",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "Natural Gas",
+		2: "Coal",
+		3: "Nuclear Power",
+		4: "Geo Thermal"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "The World's Columbian Exposition, held in Chicago in 1893, introduced what delicacy to the city?",
+	"Answer": "Hot Dogs",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "Pizza",
+		2: "Popcorn",
+		3: "Hot Dogs",
+		4: "Pretzels"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "For what phenomenon is Chicago nicknamed \"the Windy City\"?",
+	"Answer": "Politicians' bluster",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "Wind off Lake Michigan",
+		2: "Politicians' bluster",
+		3: "Tall Ship at Navy Pier",
+		4: "Quantity of harbors in Chicago"
 	}
 },
 {
-	"Question": "How many residents live in Chicago?",
-	"Answer": "2.7 million",
+	"Question": "Some say the name 'Chicago' comes from a Native American term for what food?",
+	"Answer": "Wild Onion",
 	"Choices": {
-		1: "2 million",
-		2: "2.3 million",
-		3: "2.5 million",
-		4: "2.7 million"
+		1: "Wild Onion",
+		2: "Red Potato",
+		3: "Lake Michigan whitefish",
+		4: "Lily Toad"
+	}
+},
+{
+	"Question": "What famous physicist produced the world's first controlled nuclear reaction at the University of Chicago?",
+	"Answer": "Enrico Fermi",
+	"Choices": {
+		1: "Robert Oppenheimer",
+		2: "Felix Bloch",
+		3: "Enrico Fermi",
+		4: "Sabastian Tinnes"
+	}
+},
+{
+	"Question": "Chicago's Home Insurance Building is often credited as the nation's first skyscraper. How tall was it when completed in 1885?",
+	"Answer": "Ten Stories",
+	"Choices": {
+		1: "Five Stories",
+		2: "Eight Stories",
+		3: "Ten Stories",
+		4: "Fifteen Stories"
 	}
 }]
 
 //Global Variables
 var selectedTrivia = [];
-var highScore;
+var highScore = 0;
+var correctNum;
+var intervalId;
+var time;
+var currentArrayNum = 0;
+var currentQuestionNum;
+var currentAnswer;
 
 //Functions
 function chooseQuestions(){
@@ -114,6 +140,7 @@ function chooseQuestions(){
 			if(selectedTrivia.indexOf(number) === -1){
 				selectedTrivia.push(number);
 				newNum = true;
+				//console.log(selectedTrivia)
 			}else{
 				//repeat the while loop
 			}
@@ -121,29 +148,89 @@ function chooseQuestions(){
 	}
 }
 
+function nextQuestion(){
+	console.log('next question function');
+	//time = 10;
+	//Populate the question row in html
+	currentQuestionNum++;
+	displayQuestion(selectedTrivia[currentArrayNum]);
+		//Start 30 second timer
+		//startTimer();
+	currentArrayNum++;
+	//console.log(currentArrayNum);
+}
+
 function displayQuestion(index){
-	console.log(trivia[index]);
+	currentAnswer = trivia[index].Answer;
+	$('#current-progress').html('Question '+ parseInt(currentQuestionNum) +'/10');
+	$('.question h4').html(trivia[index].Question);
+	$('.choices .text-center').empty();
+	for (var i=1; i <=4; i++){
+		$('.choices .text-center').append('<h3>'+ trivia[index].Choices[i] + '</h3>');
+	}
+
+	//console.log(trivia[index]);
+	
+}
+
+function startTimer(){
+	intervalId = setInterval(decrement, 1000);
+}
+
+function decrement(){
+	//decrease time by 1
+	time--;
+	//display the time
+	$('.time h4').html('Time Remaining '+time);
+}
+
+function stopTimer(){
+	clearInterval(intervalId);
+}
+
+function updateScores(){
+	if(correctNum > highScore){
+		highScore = correctNum;
+	}
+	$('#high-score').html('High Score: '+ highScore);
+	$('#current-score').html('Current Score: '+ correctNum);
 }
 
 //Game Logic Function
 function playGame(){
+	//hide button
+	$('.btn-play').hide();
 	//variables
-	var correctNum = 0;
-	var currentQuestionNum = 1;
+	correctNum = 0;
+	currentQuestionNum = 0;
+	currentArrayNum = 0;
 
 	//Select 10 random numbers and add them to an array
 	chooseQuestions();
-	//Begin a For Loop for 10 questions
-	for(var i = 0; i < 10; i++){
-		displayQuestion(selectedTrivia[i]);
-		//Populate the question row in html
-		//Start 30 second timer
-		//if timer runs out
-			//show answer
-			//Alert to incorrect answer
-			//Wait 5 Seconds then next question
-		//capture user guess
+	updateScores();
+	nextQuestion();
+			
+	//capture user guess
+	$(document).on('click', '.choices h3', function(){
+		var userGuess = $(this).text()
+
+		if(userGuess == currentAnswer){
+			//console.log('you win')
+			correctNum++;
+			updateScores()
+		} else {
+			//console.log('you lose')
+		}
+
+		if(currentQuestionNum === 10){
+			gameOver();
+		}else{
+			nextQuestion();
+		}
+	})
+	
 		//if user guess is correct
+
 			//show answer
 			//alert to correct answer
 			//increase correct answer counter
@@ -152,9 +239,22 @@ function playGame(){
 			//show answer
 			//alert to correct answer
 			//Wait 5 Seconds then next question
-		//update current question counter
-	}
-
+		//update current question counter	
 }
 
-playGame();
+function gameOver(){
+	$('.choices').hide();
+	$('.btn-play').show();
+}
+
+$('.btn-play').on('click', function(){
+	$('.choices').show();
+	playGame();
+})
+
+
+
+
+
+
+
